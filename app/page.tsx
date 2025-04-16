aa6ee7632b4c1c6680051c0b44ab18352034d7c7
Samuel Koigor F. Turay <96601470+Koigor97@users.noreply.github.com>
2025-04-16 22:05:33 +0200

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+
+const Home = () => {
+  return (
+    <h1 className="flex flex-col text-center text-xl mt-8">
+        DevFlow
+        <span className="text-sm text-gray-500">
+            {" "}
+            - A much more in better alternative to Stack Overflow
+        </span>
+    </h1>
+  );
+}
+
+export default Home;
