f0cac4c695215598fcfdc1244f27feb2bab6b7e4
Samuel Koigor F. Turay <96601470+Koigor97@users.noreply.github.com>
2025-04-17 13:40:57 +0200

    test styling configuration

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 const Home = () => {
   return (
-    <h1 className="flex flex-col text-center text-xl mt-8">
+    <h1 className="h1-bold flex flex-col text-center mt-8 text-light-900">
       DevFlow
-      <span className="text-sm text-white font-black">
+      <span className="text-sm text-primary-500 font-normal">
         {" "}
         - A much more in better alternative to Stack Overflow 🍍
       </span>
